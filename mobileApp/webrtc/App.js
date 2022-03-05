9640609b7d7197bdd686b6fde2adcbcf4f8d07c2
HiepTran <trandinhhiep789@gmail.com>
2022-03-05 18:41:49 +0700

    fix: commit code app

--- mobileApp/webrtc/App.js
+++ mobileApp/webrtc/App.js
@@ -1,337 +1,167 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
-
-import {
-  RTCPeerConnection,
-  RTCIceCandidate,
-  RTCSessionDescription,
-  RTCView,
-  MediaStream,
-  MediaStreamTrack,
-  mediaDevices,
-  registerGlobals
-} from 'react-native-webrtc';
-
 import io from 'socket.io-client'
+import { FlatList, StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
-const dimensions = Dimensions.get('window')
+import RoomCalling from "./src/components/RoomCalling"
 
-class App extends React.Component {
+const Separator = () => (
+  <View style={styles.separator} />
+);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 17,
+    height: 50,
+  },
+  separator: {
+    marginVertical: 4,
+  },
+});
+
+class ListUserOnline extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      localStream: null,
-      remoteStream: null,
+      myId: '',
+      nguoiGoiDen: '',
+      linkMeet: '',
+      acceptCalling: false,
+      requestCalling: false,
+      IdRequestToCall: 0,
+
+      listUserOnline: [],
+      isModalVisible: false,
     }
 
-    this.sdp
+    // DONT FORGET TO CHANGE TO YOUR URL
+    this.serviceIP = 'http://192.168.61.184:8082/webrtcPeerOnlineUser'
+
+    // this.sdp
     this.socket = null
-    this.candidates = []
+    // this.candidates = []
   }
 
   componentDidMount = () => {
-
     this.socket = io.connect(
-      'https://3824512a.ngrok.io/webrtcPeer',
+      this.serviceIP,
       {
         path: '/io/webrtc',
-        query: {}
+        query: {
+          username: ""
+        }
       }
     )
-
-    this.socket.on('connection-success', success => {
-      console.log(success)
+    this.socket.on("myId", myId => {
+      this.setState({ myId })
     })
-
-    this.socket.on('offerOrAnswer', (sdp) => {
-
-      this.sdp = JSON.stringify(sdp)
-
-      // set sdp as remote description
-      this.pc.setRemoteDescription(new RTCSessionDescription(sdp))
+    this.socket.emit('addUser', "hiep")
+    this.socket.on("getUserOnline", userList => {
+      console.log(userList)
+      userList = userList.filter((user) => user.id !== this.state.myId)
+      this.setState({ listUserOnline: userList })
     })
-
-    this.socket.on('candidate', (candidate) => {
-      // console.log('From Peer... ', JSON.stringify(candidate))
-      // this.candidates = [...this.candidates, candidate]
-      this.pc.addIceCandidate(new RTCIceCandidate(candidate))
-    })
-
-    const pc_config = {
-      "iceServers": [
-        // {
-        //   urls: 'stun:[STUN_IP]:[PORT]',
-        //   'credentials': '[YOR CREDENTIALS]',
-        //   'username': '[USERNAME]'
-        // },
-        {
-          urls: 'stun:stun.l.google.com:19302'
-        }
-      ]
-    }
-
-    this.pc = new RTCPeerConnection(pc_config)
-
-    this.pc.onicecandidate = (e) => {
-      // send the candidates to the remote peer
-      // see addCandidate below to be triggered on the remote peer
-      if (e.candidate) {
-        // console.log(JSON.stringify(e.candidate))
-        this.sendToPeer('candidate', e.candidate)
-      }
-    }
-
-    // triggered when there is a change in connection state
-    this.pc.oniceconnectionstatechange = (e) => {
-      console.log(e)
-    }
-
-    this.pc.onaddstream = (e) => {
-      debugger
-      // this.remoteVideoref.current.srcObject = e.streams[0]
-      setTimeout(() => {
-        this.setState({
-          remoteStream: e.stream
-        })
-      }, 3000);
-    }
-
-    const success = (stream) => {
-      console.log(stream.toURL())
+    this.socket.on("getLinkToCall", ({ senderId, link }) => {
+      this.setState({ isModalVisible: true })
+      console.log(senderId, link)
       this.setState({
-        localStream: stream
+        nguoiGoiDen: senderId,
+        linkMeet: link,
       })
-      this.pc.addStream(stream)
-    }
-
-    const failure = (e) => {
-      console.log('getUserMedia Error: ', e)
-    }
-
-    let isFront = true;
-    mediaDevices.enumerateDevices().then(sourceInfos => {
-      console.log(sourceInfos);
-      let videoSourceId;
-      for (let i = 0; i < sourceInfos.length; i++) {
-        const sourceInfo = sourceInfos[i];
-        if (sourceInfo.kind == "videoinput" && sourceInfo.facing == (isFront ? "front" : "environment")) {
-          videoSourceId = sourceInfo.deviceId;
-        }
-      }
-
-      const constraints = {
-        audio: true,
-        video: {
-          mandatory: {
-            minWidth: 500, // Provide your own width, height and frame rate here
-            minHeight: 300,
-            minFrameRate: 30
-          },
-          facingMode: (isFront ? "user" : "environment"),
-          optional: (videoSourceId ? [{ sourceId: videoSourceId }] : [])
-        }
-      }
-
-      mediaDevices.getUserMedia(constraints)
-        .then(success)
-        .catch(failure);
-    });
-  }
-  sendToPeer = (messageType, payload) => {
-    this.socket.emit(messageType, {
-      socketID: this.socket.id,
-      payload
     })
   }
 
-  createOffer = () => {
-    console.log('Offer')
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createOffer
-    // initiates the creation of SDP
-    this.pc.createOffer({ offerToReceiveVideo: 1 })
-      .then(sdp => {
-        // console.log(JSON.stringify(sdp))
-
-        // set offer sdp as local description
-        this.pc.setLocalDescription(sdp)
-
-        this.sendToPeer('offerOrAnswer', sdp)
-      })
+  setModalVisible = (visible) => {
+    this.setState({ isModalVisible: visible });
   }
 
-  createAnswer = () => {
-    console.log('Answer')
-    this.pc.createAnswer({ offerToReceiveVideo: 1 })
-      .then(sdp => {
-        // console.log(JSON.stringify(sdp))
-
-        // set answer sdp as local description
-        this.pc.setLocalDescription(sdp)
-
-        this.sendToPeer('offerOrAnswer', sdp)
-      })
+  makeid = (length) => {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 
-  setRemoteDescription = () => {
-    // retrieve and parse the SDP copied from the remote peer
-    const desc = JSON.parse(this.sdp)
-
-    // set sdp as remote description
-    this.pc.setRemoteDescription(new RTCSessionDescription(desc))
+  callUser = (id) => {
+    let id_room = this.makeid(7)
+    id_room = "room/" + id_room
+    this.setState({ IdRequestToCall: id_room });
+    this.setState({ requestCalling: true });
+    this.socket.emit('sendLinkToCall', {
+      senderId: 'Nam Nguyen',
+      receiverId: id,
+      link: id_room
+    })
   }
 
-  addCandidate = () => {
-    // retrieve and parse the Candidate copied from the remote peer
-    // const candidate = JSON.parse(this.textref.value)
-    // console.log('Adding candidate:', candidate)
-
-    // add the candidate to the peer connection
-    // this.pc.addIceCandidate(new RTCIceCandidate(candidate))
-
-    this.candidates.forEach(candidate => {
-      console.log(JSON.stringify(candidate))
-      this.pc.addIceCandidate(new RTCIceCandidate(candidate))
-    });
+  setStateCalling = () => {
+    this.setState({
+      acceptCalling: false,
+      requestCalling: false,
+    })
   }
-
 
   render() {
-    const {
-      localStream,
-      remoteStream,
-    } = this.state
-
-    const remoteVideo = remoteStream ?
-      (
-        <RTCView
-          key={2}
-          mirror={true}
-          style={{ ...styles.rtcViewRemote }}
-          objectFit='contain'
-          streamURL={remoteStream && remoteStream.toURL()}
-        />
-      ) :
-      (
-        <View style={{ padding: 15, }}>
-          <Text style={{ fontSize: 22, textAlign: 'center', color: 'white' }}>Waiting for Peer connection ...</Text>
-        </View>
-      )
-
     return (
-
-      <SafeAreaView style={{ flex: 1, }}>
-        {/* <StatusBar backgroundColor="blue" barStyle={'dark-content'} /> */}
-        <View style={{ ...styles.buttonsContainer }}>
-          <View style={{ flex: 1, }}>
-            <TouchableOpacity onPress={this.createOffer}>
-              <View style={styles.button}>
-                <Text style={{ ...styles.textContent, }}>Call</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View style={{ flex: 1, }}>
-            <TouchableOpacity onPress={this.createAnswer}>
-              <View style={styles.button}>
-                <Text style={{ ...styles.textContent, }}>Answer</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{ ...styles.videosContainer, }}>
-          <ScrollView style={{ ...styles.scrollView }}>
-            <View style={{
-              flex: 1,
-              width: '100%',
-              backgroundColor: 'black',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-              {remoteVideo}
-            </View>
-          </ScrollView>
-          <View style={{
-            position: 'absolute',
-            zIndex: 1,
-            bottom: 10,
-            right: 10,
-            width: 100, height: 200,
-            backgroundColor: 'black', //width: '100%', height: '100%'
-          }}>
-            <View style={{ flex: 1 }}>
-              <TouchableOpacity onPress={() => localStream._tracks[1]._switchCamera()}>
+      <View style={styles.container}>
+        {this.state.requestCalling || this.state.acceptCalling ? (
+          this.state.requestCalling ?
+            <RoomCalling setStateCalling={this.setStateCalling} IdRoom={this.state.IdRequestToCall} /> : <RoomCalling setStateCalling={this.setState} IdRoom={this.state.linkMeet} />) :
+          <View>
+            {this.state.isModalVisible ?
+              <View>
+                <Text style={{ color: "orange", fontWeight: 'bold', fontSize: 20 }}>
+                  Bạn đang có cuộc gọi tới từ {this.state.nguoiGoiDen}
+                </Text>
+                <Separator />
+                <Separator />
                 <View>
-                  <RTCView
-                    key={1}
-                    zOrder={0}
-                    objectFit='cover'
-                    style={{ ...styles.rtcView }}
-                    streamURL={localStream && localStream.toURL()}
+                  <Button
+                    title="Trả lời"
+                    onPress={() => {
+                      this.setState({ isModalVisible: false })
+                      this.setState({ acceptCalling: true })
+                    }}
+                  />
+                  <Separator />
+                  <Button
+                    color="#f194ff"
+                    title="Từ chối"
+                    onPress={() => this.setState({ isModalVisible: false })}
                   />
                 </View>
-              </TouchableOpacity>
+              </View>
+              :
+              <Text></Text>}
+            <Separator />
+            <View>
+              <Text style={{ color: "blue", fontWeight: 'bold', fontSize: 20 }}>Mã ID của bạn: </Text>
+              <Text style={styles.item}>{this.state.myId.substring(this.state.myId.indexOf("#"), this.state.myId.length)}</Text>
+              <Text style={{ color: "green", fontWeight: 'bold', fontSize: 20 }}>Danh sách người dùng đang online: </Text>
+              <FlatList
+                data={this.state.listUserOnline}
+                renderItem={({ item }) =>
+                  <Text onPress={() => { this.callUser(item.id) }} style={styles.item}>
+                    {item.id.substring(item.id.indexOf("#"), item.id.length)}
+                  </Text>
+                }
+              />
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    );
+        }
+      </View>
+    )
   }
-};
 
-const styles = StyleSheet.create({
-  buttonsContainer: {
-    flexDirection: 'row',
-  },
-  button: {
-    margin: 5,
-    paddingVertical: 10,
-    backgroundColor: 'lightgrey',
-    borderRadius: 5,
-  },
-  textContent: {
-    fontFamily: 'Avenir',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  videosContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  rtcView: {
-    width: 100, //dimensions.width,
-    height: 200,//dimensions.height / 2,
-    backgroundColor: 'black',
-  },
-  scrollView: {
-    flex: 1,
-    // flexDirection: 'row',
-    backgroundColor: 'teal',
-    padding: 15,
-  },
-  rtcViewRemote: {
-    width: dimensions.width - 30,
-    height: 200,//dimensions.height / 2,
-    backgroundColor: 'black',
-  }
-});
+}
 
-export default App;
+export default ListUserOnline;
